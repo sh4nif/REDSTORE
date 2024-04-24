09fab32a8e70d8ff4e7096a19d97ba4f1813500d
sh4nif <shanifsanu88647@gmail.com>
2024-04-24 22:49:46 +0530

    first commit

---
+++ assets/scripts.js
@@ -0,0 +1,40 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+
+
+  // =======================accounts=========================//
+
+
+  const forms = document.querySelector(".forms"),
+  pwShowHide = document.querySelectorAll(".eye-icon"),
+  links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+eyeIcon.addEventListener("click", () => {
+    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+    
+    pwFields.forEach(password => {
+        if(password.type === "password"){
+            password.type = "text";
+            eyeIcon.classList.replace("bx-hide", "bx-show");
+            return;
+        }
+        password.type = "password";
+        eyeIcon.classList.replace("bx-show", "bx-hide");
+    })
+    
+})
+})      
+
+links.forEach(link => {
+link.addEventListener("click", e => {
+   e.preventDefault();
+   forms.classList.toggle("show-signup");
+})
+})
